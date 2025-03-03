b5cd30108bfe7641ed739977155efa4d789a50d5
painkiller102k <mstislavlandsh@gmail.com>
2025-03-03 21:18:45 +0200

    Vigade Otsing -2 done

--- Vigade otsing-2.py
+++ Vigade otsing-2.py
@@ -1,55 +1,64 @@
+from time import *
+
 print("*** Arvude mäng ***")
 print()
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 while 1:
     try:
-        a = (abs(int(input("Sisestage täisarv => ")))) # )
+        a = abs(int(input("Sisestage täisarv => "))) #---> )
         break
     except ValueError:
         print("See ei ole täisarv")
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-if a==0:
+if a == 0:
     print("Nulliga on möttetu töö")
 else:
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("Loendame, mitu paarilist ja mitu paaritut numbrit on numbris.")
     print()
-    c=b=a # ---> c=b=a
-    paaris = 0 # ---> =
-    paaritu = 0 # ---> =
-while b > 0: # ---> : 
-            if b % 2 == 0: # ----> ==
-                    paaris =+ 1
-            else:
-                    paaritu =+ 1
-                    b = b // 10
+    c = b = a # ---> c=b=a
+    paaris = 0
+    paaritu = 0
+    while b > 0: # ---> : 
+        if b % 2 == 0: # ----> ==
+            paaris += 1
+        else:
+            paaritu += 1
+        b = b // 10
     
-print("Paarisarvud:",paaris) # ----> ,paaris
-print("Paarituarvud:",paaritu) # ----> ,paaritu
-print()
-#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-print("*Pöörake* sisestatud number ümber")
-print()
-b=0
-while a > 0: # ----> :
-          number = a % 10
-          a = a // 10
-          b = b * 10
-          b =+ number
-print("*Pöörake* arv'", b)
-print()
-#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-print("Syracuse'i hüpoteesi testimine.") # ---> (
-print()
-if c % 2 == 0: # ---> ==
-    print("c on paariline arv. Jagage 2ga.")
-else:
-    print("c on paaritu arv. Korrutage 3, lisage 1 ja jagage 2ga.")
-    while c != 1:
-            if c % 2 == 0: # ---> ==
-                    c == c / 2
-            else:
-                    c == (3*c + 1) / 2
-            print("c", end="") # ----> "c" ----> end=""
+    print("Paarisarvud:", paaris) # ----> ,paaris
+    print("Paarituarvud:", paaritu) # ----> ,paaritu
     print()
-    print("Hüpotees on õige.") # ---> ""
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    print("*Ümberpöörame* sisestatud arv: ",c) # ----> ,c
+    print()
+    b=0
+    while a > 0:
+        number = a % 10
+        a = a // 10
+        b = b * 10
+        b += number # ----> +=
+    print("*Ümberpööratud* arv: ", b)
+    print()
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print("Tõestame teoreem")
+print()
+if c % 2 ==0:
+    print(c," - Paaris arv, Jagame 2.") # ----> ,c
+else:
+    print(c," - Paaritu arv. Korrutame 3, liidame 1 ja jagame 2.") # ----> ,c
+    for i in range(50):
+        print(">", end="")
+        sleep(0.5)
+    print()
+    while c != 1:
+        sleep(0.5) # 0.5 sekund
+        if c % 2 == 0:
+            print('{:>4}'.format(round(c))," - Paaris arv, Jagame 2.")
+            c = c / 2
+        else:
+            print('{:>4}'.format(round(c))," - Paaritu arv. Korrutame 3, liidame 1 ja jagame 2.")
+            c = (3*c + 1) / 2
+
+    print('{:>4}'.format(round(c)),"- Teoreem on tõestatud")
+    print()
